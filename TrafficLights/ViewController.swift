f87fc889cbf16636d6e0e6824dee893ba5c19e53
Evgeny Zhurilin <zhurilin2014@yandex.ru>
2025-03-21 01:50:28 +0200

    Setting up elements

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColorTrafficLights: UIView!
+    
+    @IBOutlet var yellowColorTrafficLights: UIView!
+    
+    @IBOutlet var greenColorTrafficLights: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorTrafficLights.alpha = 0.3
+        yellowColorTrafficLights.alpha = 0.3
+        greenColorTrafficLights.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 10
+        redColorTrafficLights.layer.cornerRadius = 75
+        yellowColorTrafficLights.layer.cornerRadius = 75
+        greenColorTrafficLights.layer.cornerRadius = 75
     }
 
-
+    @IBAction func startButtonDitTapped(_ sender: UIButton) {
+        
+        redColorTrafficLights.alpha = 1
+        sender.setTitle("NEXT", for: .normal)
+        
+    
+    }
+    
 }
 
