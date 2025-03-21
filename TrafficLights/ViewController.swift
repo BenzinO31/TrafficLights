e76d00c0caa480872c37472997bb4f30b25ba824
Evgeny Zhurilin <zhurilin2014@yandex.ru>
2025-03-22 01:32:42 +0200

    Adding logic to the button

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet var redColorTrafficLights: UIView!
@@ -16,6 +20,8 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorTrafficLights: UIView!
     
     @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,11 +36,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func startButtonDitTapped(_ sender: UIButton) {
-        
-        redColorTrafficLights.alpha = 1
+        onClikButton()
         sender.setTitle("NEXT", for: .normal)
         
     
+    }
+    
+    private func onClikButton() {
+        
+        switch currentLight {
+        case .red:
+            greenColorTrafficLights.alpha  = 0.3
+            redColorTrafficLights.alpha = 1
+            currentLight = CurrentLight.yellow
+            
+        case .yellow:
+            redColorTrafficLights.alpha = 0.3
+            yellowColorTrafficLights.alpha = 1
+            currentLight = CurrentLight.green
+            
+        case .green:
+            yellowColorTrafficLights.alpha = 0.3
+            greenColorTrafficLights.alpha = 1
+            currentLight = CurrentLight.red
+        }
     }
     
 }
